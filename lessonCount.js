0a8cd6c24b72defab5f8cdb193578dd396bb3497
Ryan Boddy <avidryanb@gmail.com>
2025-01-11 00:48:04 -0500

    Update lessonCount.js

--- lessonCount.js
+++ lessonCount.js
@@ -4,6 +4,6 @@ let numberRows = document.querySelectorAll(".lessontable-row-number");
 let numbersArray = Array.from(numberRows);
 for (let lessonNo in numbersArray) {
   if (numbersArray[lessonNo].innerText == currentLesson) {
-    console.log(`You are on lesson number ${parseInt(lessonNo) + 1} out of ${numbersArray.length + 1}! That's ${(parseInt(lessonNo) + 1) / (numbersArray.length + 1) * 100}% of the way done! Keep going!`);
+    console.log(`You are on lesson number ${parseInt(lessonNo) + 1} out of ${Math.round(numbersArray.length + 1}! That's ${(parseInt(lessonNo) + 1) / (numbersArray.length + 1) * 100)}% of the way done! Keep going!`);
   }
 }
