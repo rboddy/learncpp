efcc07ac244201c90b12ce1c22a763434294d69b
rboddy <avidryanb@gmail.com>
2025-01-12 23:04:05 -0500

    Adding in linkage updates from earlier today

--- 20_Linkage/main.cpp
+++ 20_Linkage/main.cpp
@@ -2,6 +2,9 @@
 
 static int gLocalIntMain = 8;
 
+extern int gExternalInt;
+// The above shouldn't really be done since it's non constant
+
 static int returnANumber() { return 5; }
 
 int main() {
@@ -10,8 +13,11 @@ int main() {
   // static keyword, we ensure the global int declared above only has linkage in
   // this file. Further more, I can't access the identifier from other. You get
   // a compile error about scope if you try
-  std::cout << gGlobalInt << '\n';
+  std::cout << gLocalIntMain << '\n';
+
+  // You can also externally link a global variable by using the extern variable
+  // as shown above
 }
 
 // You can also make functions static. This function will not be accessible
-// anywhere else
+// anywhere else. However non-contant globals have external linkage by default
