8f5d94499831954ddde75043474f87ebf4407006
rboddy <avidryanb@gmail.com>
2025-01-21 13:33:34 -0500

    Adding more details to the classes lesson

--- 34_Classes/main.cpp
+++ 34_Classes/main.cpp
@@ -23,6 +23,34 @@ class Date {
   }
 };
 
+class Character {
+  // not needed explicitly, but it's good to have
+ private:
+  int m_health{10};
+  double m_speed{100.00};
+  double m_jumpHeight{10.0};
+
+ public:
+  int getHealth() const { return m_health; }
+  void setHealth(int health) { m_health = health; }
+
+  double getSpeed() const { return m_speed; }
+  void setSpeed(double speed) { m_speed = speed; }
+
+  double getJumpHeight() const { return m_jumpHeight; }
+  void setJumpHeight(double jumpHeight) { m_jumpHeight = jumpHeight; }
+};
+
+class Foo {
+ private:
+  int m_x{};
+  int m_y{};
+
+  // Constructor below with member initializer list
+ public:
+  Foo(int x, int y) : m_x{x}, m_y{y} { std::cout << "Foo constructed \n"; }
+};
+
 void printEmployeeId(const Employee& employee) {
   std::cout << employee.m_id << '\n';
 };
@@ -39,4 +67,15 @@ int main() {
   // The below is an example of making a class object constant
   const Date today{2025, 1, 20};
   today.print();
+
+  // Since class members are private by default, and should usually remain that
+  // way, we can use getters and setters to work with them:
+
+  Character c{};
+  c.setSpeed(200.00);
+  std::cout << "The players speed is " << c.getSpeed() << '\n';
+
+  // We can't implement classes as aggregators due to private members. However,
+  // we can use constructors
+  Foo foo{6, 7};  // is NOT an aggregate. This calls foos constructor
 }
